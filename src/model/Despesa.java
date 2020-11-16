2a0b47cd6fe697e9e73d2336f22c33f3f785f874
Iagopeppe <iagotambosi@hotmai.com>
2020-11-15 22:09:09 -0300

    - GUI CadastroDespesa adicionada - Segue o mesmo formato da CadastroReceita

--- src/model/Despesa.java
+++ src/model/Despesa.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -20,7 +23,27 @@ public class Despesa extends Receita {
     
     @Override
     public void salvarDados() {
-        
+        String dadosParaSalvar = this.getDataLancamento() + ";" + this.getValor() + ";" + this.tipoDespesa; // Concatena os dados que serão salvos no arquivo
+        System.out.println("Valores concatenados: ");
+        System.out.println(dadosParaSalvar);
+
+        // Objetos para manipulação de arquivos
+        FileWriter fileWriter;
+        BufferedWriter bufferedWriter;
+        try {
+            // Carrega o arquivo. O parâmetro 'true' indica que o arquivo carregado NÃO deverá ser sobreescrito, e sim feito a concatenação de todos os dados
+            fileWriter = new FileWriter("test.txt", true);
+            
+            bufferedWriter = new BufferedWriter(fileWriter);
+
+            bufferedWriter.write(dadosParaSalvar); // Escreve os dados no arquivo
+            bufferedWriter.newLine(); // Pula uma linha no arquivo
+            bufferedWriter.flush(); // Salva as alterações feitas no arquivo
+
+            bufferedWriter.close(); // Fecha o arquivo
+        } catch (IOException ex) {
+            System.out.println("Erro ao salvar os dados da entrada!");
+        }
     }
 
     @Override
